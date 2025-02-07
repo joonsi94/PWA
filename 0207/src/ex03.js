c21f95ceeeeba9abc82f91c18a46cfd0cbce669c
DESKTOP-29VI7GO\user <wnstjd637@gmail.com>
2025-02-07 12:38:54 +0900

    0207

---
+++ 0207/src/ex03.js
@@ -0,0 +1,22 @@
+var x = 2;
+
+//0이 아닌 모든 수는 true
+//0은 false
+if (x % 2) {
+    console.log("참");
+} else {
+    console.log("거짓");
+}
+
+if (true) {
+    console.log("한줄");
+    console.log("두줄");
+    console.log("세줄");
+}
+
+if (false) {
+} else {
+    console.log("한개");
+    console.log("두개");
+    console.log("세개");
+}
---
+++ 0207/src/ex05.js
@@ -0,0 +1,13 @@
+var a = 0;
+var b = 1;
+
+var c = a&&b;
+
+console.log(c);
+
+var c = a||b;
+
+console.log(c);
+
+var d =a&&"테스트";
+console.log(`d=${d}`);
