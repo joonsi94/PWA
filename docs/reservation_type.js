db77458c279e2944152cf6907a09da137e2b9250
DESKTOP-29VI7GO\user <wnstjd637@gmail.com>
2025-02-18 17:41:13 +0900

    no message

--- docs/reservation_type.js
+++ docs/reservation_type.js
@@ -1,29 +1,28 @@
-var Select = document.querySelector('#type');
-var $etc1 = document.querySelector('#etc1');
-var $etc2 = document.querySelector('#etc2');
-var $divService = document.querySelector('#div-service');
+const $select = document.querySelector('#type');
+const $etc1 = document.querySelector('#etc1');
+const $etc2 = document.querySelector('#etc2');
+const $divService = document.querySelector('#div-service');
 
 const $confirmReserve = document.querySelector('#confirm-reserve');
 const $step01 = document.querySelector('#step01');
 const $step02 = document.querySelector('#step02');
-// div h1~ img
-// mouseover mouseout click change
-Select.addEventListener('change', function (e) {
-    // alert(e.target);
-    // alert('이게 되네'+e.target.value);
-    if (e.target.value === 'jim-keep') {
+
+$select.addEventListener('change', function (e) {
+    if(e.target.value === 'jim-keep'){
         $etc1.classList.remove('hidden');
         $etc2.classList.add('hidden');
 
         $divService.classList.remove('h-20');
         $divService.classList.add('h-100');
-    } else if (e.target.value === 'jim-move') {
-        $etc2.classList.remove('hidden');
+    }
+    else if(e.target.value === 'jim-move'){
         $etc1.classList.add('hidden');
+        $etc2.classList.remove('hidden');
 
         $divService.classList.remove('h-20');
         $divService.classList.add('h-100');
-    } else {
+    }
+    else {
         $etc1.classList.add('hidden');
         $etc2.classList.add('hidden');
 
@@ -36,4 +35,3 @@ $confirmReserve.addEventListener('click', function (e) {
     $step01.classList.add('hidden');
     $step02.classList.remove('hidden');
 });
-
